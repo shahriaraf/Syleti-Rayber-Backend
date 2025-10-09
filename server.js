3f8f567cfa7d4dd1b5b5160702c90603f11fadea
Shoumo Shahriar Araf <shahriaraf01@gmail.com>
2025-10-10 00:18:50 +0600

    added things

--- server.js
+++ server.js
@@ -3,25 +3,39 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const database = require('./middleware/database'); // Import the new middleware
+const database = require('./middleware/database');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// Middleware setup
-app.use(cors());
-app.use(express.json());
-app.use(database); // Apply the database middleware to all requests
+const whitelist = ['https://www.merrigemedia.com', 'https://merrigemedia.com'];
 
-// Route setup
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true); 
+    } else {
+      callback(new Error('Not allowed by CORS')); 
+    }
+  },
+  credentials: true,
+};
+
+
+
+app.use(cors(corsOptions)); 
+app.use(express.json());
+app.use(database);
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
-// Root route for health check
+
 app.get('/', (req, res) => {
   res.send('Syleti Rayber API is running successfully on Vercel!');
 });
 
-// Export the app for Vercel's serverless environment
+
 module.exports = app;
